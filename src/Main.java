79c50028945865556b3d08502a018af0930dc086
YvesKalume <yvkalume@gmail.com>
2022-07-05 10:25:18 +0200

    lire les données aux id impaires

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ class Main {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/esis", "root", "");
             Statement monInstruction = connection.createStatement();
-            String requete = "Select * From etudiant";
+            String requete = "Select * From etudiant where id % 2 != 0 ";
             ResultSet monResultat = monInstruction.executeQuery(requete);
 
+
             while (monResultat.next()) {
+                System.out.println(monResultat.getInt("id"));
                 System.out.println(monResultat.getString("nom"));
+                System.out.println(monResultat.getString("matricule"));
             }
 
             connection.close();
